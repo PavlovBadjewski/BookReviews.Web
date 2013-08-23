9e672896785e8a6755faf7e57f98c20fcbcb49bf
Pavlov Badjewski <pavlov.badjewski@gmail.com>
2013-08-22 20:21:15 -0700

    Genre fix.  Added link to tweets

--- Scripts/Custom/GlobalData.js
+++ Scripts/Custom/GlobalData.js
@@ -1,6 +1,6 @@
 ﻿angular.module('genreServices', ['ngResource']).
     factory('Genres', function ($resource) {
-        return $resource('http://api.book.reviews/Api/GlobalData/Genres', {}, {
+        return $resource(BRM.WebApi + 'GlobalData/Genres', {}, {
             query: { method: 'GET', params: {}, isArray: true }
         });
     });
